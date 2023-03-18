43c68990d71a18623e3d2d5b74655fc49d838688
Gor Melkonyan <gormelkonyan005@gmail.com>
2023-03-18 12:06:03 +0400

    first commit

---
+++ packages/frontend/src/app/nx-welcome.jsx
@@ -0,0 +1,16 @@
+import './css/nx-welcome.css'
+
+
+export default function Project() {
+  
+
+  return(
+    <div className="Project">
+      <header>
+        <div className="header-blocks1"></div>
+        <div className="header-blocks2"></div>
+      </header>
+
+    </div>
+  )
+}
